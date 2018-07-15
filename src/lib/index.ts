b4cfab48975e5109256ff13eb5dd525fc649d72c
Ulises Santana <ulisesantana@gmail.com>
2018-07-15 20:25:50 +0100

    [WIP] Starts with strict mode

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,44 @@
-export default function objectComparer(obj: object, model: object): boolean {
-  if (Object.keys(obj).length > Object.keys(model).length) {
+export default function objectComparer(obj: object, model: object, strict=false): boolean {
+  return Object.entries(model).every(([key, value]) => {
+    if (value instanceof Object) {
+      return objectComparer(obj[key], model[key]);
+    } else if (hasProperty(obj,model,key,strict) || propertyIsOptional(model,key)) {
+      return true
+    }
     return false;
-  } else {
-    return Object.entries(model).every(([key, value]) => {
-      if (value instanceof Object) {
-        return objectComparer(obj[key], model[key]);
-      } else if (obj.hasOwnProperty(key)) {
-        return true
-      }
-      return false;
-    });
+  });
+}
+
+function hasProperty(obj: object, model: object, key: string, strict: boolean): boolean{
+  return strict 
+    ? obj.hasOwnProperty(key) && checkTypes(processValue(model[key]), obj[key])
+    : obj.hasOwnProperty(key);
+}
+
+function propertyIsOptional(model: object, key: string){
+  return model[key] && model[key] instanceof String && model[key].includes('?')
+}
+
+function checkTypes(types: any[], value): boolean{
+  return types.every((v) => checkType(value, v))
+}
+
+function checkType(value, modelValue): boolean{
+  switch (modelValue){
+    case 'string':
+      return value instanceof String;
+    case 'number':
+      return value instanceof Number;
+    case 'array':
+      return value instanceof Array;
+    case 'object':
+    default:
+      return value instanceof Object || typeof value === 'object';
   }
+}
+
+function processValue(string): string[] | null[]{
+  return string instanceof String 
+    ? string.toLowerCase().replace(/\?/g,'').split('|')
+    : [null];
 }
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -8,18 +8,46 @@ describe('Basic tests', function () {
     assert.deepStrictEqual(oc(obj, obj), true);
   });
 
-  it('Should reject objects with different length', function () {
+  it('Should recognize objects with optional properties', function () {
+    const model = { a: null, b: null, c: null, d: '?' };
     const obj1 = { a: 10, b: 10, c: 10 };
     const obj2 = { a: 10, b: 10, c: 10, d: null };
-    assert.deepStrictEqual(oc(obj1, obj2), false);
-    assert.deepStrictEqual(oc(obj2, obj1), false);
+    assert.deepStrictEqual(oc(model, obj2), true);
+    assert.deepStrictEqual(oc(model, obj1), true);
   });
 
   it('Should recognize nested objects', function () {
+    const model = { a: null, b: { c: null, d: { f: [{ g: null }, { m: null }] } } };
     const obj1 = { a: 10, b: { c: null, d: { f: [{ g: true }, { m: false }] } } };
     const obj2 = { a: 10, b: { c: null, d: { f: [{ g: true }, { M: true }] } } };
-    assert.deepStrictEqual(oc(obj1, obj2), false);
-    assert.deepStrictEqual(oc(obj1, obj1), true);
+    assert.deepStrictEqual(oc(model, obj2), false);
+    assert.deepStrictEqual(oc(model, obj1), true);
   });
 
+
+  describe('Strict mode (also compare the values types)', function () {
+    const strict = true;
+
+    it('Should recognize same plain object with exact same types as model', function () {
+      const model = { a: 'number', b: 'string', c: 'array' };
+      const obj = { a: 10, b: 'meh', c: [] };
+      assert.deepStrictEqual(oc(model, obj, strict), true);
+    });
+
+    it('Should recognize objects with optional properties', function () {
+      const model = { a: null, b: null, c: null, d: '?' };
+      const obj1 = { a: 10, b: 10, c: 10 };
+      const obj2 = { a: 10, b: 10, c: 10, d: null };
+      assert.deepStrictEqual(oc(model, obj2), true);
+      assert.deepStrictEqual(oc(model, obj1), true);
+    });
+
+    it('Should recognize nested objects', function () {
+      const model = { a: null, b: { c: null, d: { f: [{ g: null }, { m: null }] } } };
+      const obj1 = { a: 10, b: { c: null, d: { f: [{ g: true }, { m: false }] } } };
+      const obj2 = { a: 10, b: { c: null, d: { f: [{ g: true }, { M: true }] } } };
+      assert.deepStrictEqual(oc(model, obj2), false);
+      assert.deepStrictEqual(oc(model, obj1), true);
+    });
+  });
 });
